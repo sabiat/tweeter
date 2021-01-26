d14e958765e44c26814921b5a35cae8366a7fe7c
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-26 22:11:09 +0000

    Added code to make counter turn red after 140 characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
     let remainingChar = 140 - $(this).val().length;
     let counter = $(this).closest('form').find('.counter');
     counter.val(remainingChar);
+    console.log(counter.val() < 0)
+    if ($('.counter').val() < 0) {
+      $('.counter').addClass('red');
+    } else {
+      $('.counter').removeClass('red');
+    }
   });
+
 
 });
