6385eae8d223495fbd205a2752d6484e67e5a127
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-26 21:27:12 +0000

    Implemented character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,11 @@
-console.log('hello')
+$(document).ready(function() {
+  // --- our code goes here ---
+
+
+  $("#tweet-text").on('input', function() {
+    let remainingChar = 140 - $(this).val().length;
+    let counter = $(this).closest('form').find('.counter');
+    counter.val(remainingChar);
+  });
+
+});
