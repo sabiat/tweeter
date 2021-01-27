0c61f60adcb7d6cf7f1d004f5d4c3c75faf609f0
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-27 17:19:00 +0000

    Added styling to avatar in all tweets section

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,52 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  
+const createTweetElement = function(tweetObj) {
+  const $tweet = `
+  <article class="tweet">
+  <header>
+    <div class="left-side">
+      <img src=${tweetObj.user.avatars} alt="usericon">
+      <span>${tweetObj.user.name}</span>
+    </div>
+    <p class="hidden-username">${tweetObj.user.handle}</p>
+  </header>
+  <div class="tweet-feed">${tweetObj.content.text}</div>
+  <footer>
+    <p>${tweetObj["created_at"]}</p>
+    <div class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+    </div>
+  </footer>
+  </article>
+`;
+
+  return $tweet;
+}
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#all-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
+
