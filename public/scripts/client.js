e077fc98ca19d870c97157f5c264e6d7b769c69b
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-27 20:37:08 +0000

    Added functionality to do POST request with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,38 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetObj) {
+  const timeSince = moment(tweetObj.created_at).fromNow();
+  let $tweet = `
+  <article class="tweet">
+  <header>
+    <div class="left-side">
+      <img src=${tweetObj.user.avatars} alt="usericon">
+      <span>${tweetObj.user.name}</span>
+    </div>
+    <p class="hidden-username">${tweetObj.user.handle}</p>
+  </header>
+  <div class="tweet-feed">${tweetObj.content.text}</div>
+  <footer>
+    <p>${timeSince}</p>
+    <div class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+    </div>
+  </footer>
+  </article>
+`;
+  return $tweet;
+};
+
+const renderTweets = function(tweetsArr) {
+  for (let tweet of tweetsArr) {
+    const $newTweet = createTweetElement(tweet);
+    $('#all-tweets').append($newTweet);
+  }
+};
+
 $(document).ready(function() {
 
   // Fake data taken from initial-tweets.json
@@ -32,39 +64,33 @@ $(document).ready(function() {
     }
   ]
 
-  const createTweetElement = function(tweetObj) {
-    const timeSince = moment(tweetObj["created_at"]).fromNow();
-    let $tweet = `
-    <article class="tweet">
-    <header>
-      <div class="left-side">
-        <img src=${tweetObj.user.avatars} alt="usericon">
-        <span>${tweetObj.user.name}</span>
-      </div>
-      <p class="hidden-username">${tweetObj.user.handle}</p>
-    </header>
-    <div class="tweet-feed">${tweetObj.content.text}</div>
-    <footer>
-      <p>${timeSince}</p>
-      <div class="icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-      </div>
-    </footer>
-    </article>
-  `;
-    return $tweet;
-  };
-
-
-  const renderTweets = function(tweetsArr) {
-    for (let tweet of tweetsArr) {
-      const $newTweet = createTweetElement(tweet);
-      $('#all-tweets').append($newTweet);
-    }
-  };
   renderTweets(data);
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    console.log('clicked');
+    const tweet = $(this).serialize();
+    console.log(tweet);
+
+    $.ajax({
+      url: "/tweets/",
+      method: 'POST',
+      data: tweet
+    })
+      .done(function(data) {
+        console.log('Success:', data);
+      })
+  });
+
+  const loadTweets = function() {
+
+    $.ajax("/tweets/", {method: 'GET'})
+      .done(function(data) {
+        console.log(data);
+      })
+  };
+
+  loadTweets();
 
 });
 
