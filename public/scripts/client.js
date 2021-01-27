1d15f4701793979da9163b2ef684b24e41195c36
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-27 17:44:56 +0000

    Implemented functionality for dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,51 +5,68 @@
  */
 
 $(document).ready(function() {
-  
-const createTweetElement = function(tweetObj) {
-  const $tweet = `
-  <article class="tweet">
-  <header>
-    <div class="left-side">
-      <img src=${tweetObj.user.avatars} alt="usericon">
-      <span>${tweetObj.user.name}</span>
-    </div>
-    <p class="hidden-username">${tweetObj.user.handle}</p>
-  </header>
-  <div class="tweet-feed">${tweetObj.content.text}</div>
-  <footer>
-    <p>${tweetObj["created_at"]}</p>
-    <div class="icons">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="far fa-heart"></i>
-    </div>
-  </footer>
-  </article>
-`;
 
-  return $tweet;
-}
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const $tweet = createTweetElement(tweetData);
+  const createTweetElement = function(tweetObj) {
+    let $tweet = `
+    <article class="tweet">
+    <header>
+      <div class="left-side">
+        <img src=${tweetObj.user.avatars} alt="usericon">
+        <span>${tweetObj.user.name}</span>
+      </div>
+      <p class="hidden-username">${tweetObj.user.handle}</p>
+    </header>
+    <div class="tweet-feed">${tweetObj.content.text}</div>
+    <footer>
+      <p>${tweetObj["created_at"]}</p>
+      <div class="icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>
+    </footer>
+    </article>
+  `;
+    return $tweet;
+  };
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#all-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  const renderTweets = function(tweetsArr) {
+    for (let tweet of tweetsArr) {
+      const newTweet = createTweetElement(tweet);
+      console.log(newTweet)
+      $('#all-tweets').append(newTweet);
+    }
+  };
+
+  renderTweets(data);
+
 
 });
 
