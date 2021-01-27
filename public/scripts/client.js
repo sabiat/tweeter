2a88e21a05b4c8e351f7c75aad8bb82095402bc5
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-27 20:48:00 +0000

    Added GET request with ajax to fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,39 +38,9 @@ const renderTweets = function(tweetsArr) {
 
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
-
   $('form').on('submit', function(event) {
     event.preventDefault();
-    console.log('clicked');
     const tweet = $(this).serialize();
-    console.log(tweet);
 
     $.ajax({
       url: "/tweets/",
@@ -82,11 +52,14 @@ $(document).ready(function() {
       })
   });
 
+  //makes Ajax GET request and receive array of tweets then call renderTweets to load tweets on success
   const loadTweets = function() {
-
     $.ajax("/tweets/", {method: 'GET'})
       .done(function(data) {
-        console.log(data);
+        renderTweets(data);
+      })
+      .fail(function () {
+        alert('error');
       })
   };
 
